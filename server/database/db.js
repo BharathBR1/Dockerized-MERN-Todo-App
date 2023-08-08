fec6855f787f3654f8464c6cdd346a6953bdca62
BharathBR1 <bharathbr1821@gmail.com>
2023-08-08 21:38:47 +0530

    done

---
+++ server/database/db.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+
+    // const MONGODB_URI = `mongodb+srv://bharathbr1821:YoXzbtuhQQ0I7DOJ@imaginate.nmcangf.mongodb.net/?retryWrites=true&w=majority`;
+    const MONGODB_URI = `mongodb://db/TODO-LIST`;
+
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
