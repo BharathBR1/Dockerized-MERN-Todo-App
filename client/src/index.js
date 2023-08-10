9fc638e72218723c21c20f6ad84c80de2dbf2ecb
BharathBR1 <bharathbr1821@gmail.com>
2023-08-10 06:09:38 +0530

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 import store from './redux/store';
 
@@ -17,8 +17,3 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
